fa6efae82644937a9ada6033f9ef281a39fbfaf7
Anh Tu Bui <40556943+anhtubui1204@users.noreply.github.com>
2020-12-27 07:58:16 +0700

    add express server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'build')))
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build/index.html'), function(err) {
+        if (err) {
+        res.status(500).send(err)
+        }
+    })
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server running on ${port}`)
+});

2c3e64d4118ac3d717120661346f332391b715d8
Anh Tu Bui <40556943+anhtubui1204@users.noreply.github.com>
2020-12-27 08:24:33 +0700

    fix server.js
    only serve build in production mode

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const path = require('path');
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'build')))
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build/index.html'), function(err) {
-        if (err) {
-        res.status(500).send(err)
-        }
+if(process.env.NODE_ENV === 'production') {
+    app.get('/*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'build/index.html'), function(err) {
+            if (err) {
+            res.status(500).send(err)
+            }
+        })
     })
-})
+}
 
 const port = process.env.PORT || 5000;
 
